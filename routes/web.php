eeb11cc793a2850f2abd03c474af779ed40e944f
Pruek <pruek@testusers-iMac.local>
2017-11-03 23:32:52 +0700

    first commit basic laravel

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+
+*/
+use Illuminate\Support\Facades\DB;
+
+Route::get('/', function () {
+    $data = DB::connection('mysql')->select('select * from gallery where 1');
+    return view('welcome', ['users' => $data[0]]);
+});
+
+Route::get('/uploadfile','UploadFileController@index');
+Route::post('/uploadfile','UploadFileController@showUploadFile');
